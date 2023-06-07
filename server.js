144b894a4c5d3dcfdf96589f8b03a2d42174339e
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-07 14:20:56 +0300

    Integrate Argon2 for pass storage

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const crypto = require('crypto');
+const argon2 = require('argon2');
 
 const app = express();
 class User {
   static createdCount = 0;
-  constructor(name, password) {
+  constructor(name, passHash) {
     this.id = (++User.createdCount).toString().padStart(8, '0');
     this.name = name;
-    this.password = password;
+    this.passHash = passHash;
     this.facesDetected = 0;
     this.registrationDate = new Date();
   }
 }
+
 const database = {
-  users: [new User('Jonkata', 'strongestpass')],
+  users: [],
 };
+
+argon2.hash('strongestpass').then(hash => {
+  database.users.push(new User('Jonkata', hash));
+});
 
 app.use(express.json());
 
@@ -25,23 +30,25 @@ app.get('/', (req, res) => {
 app.get('/profile/:user', (req, res) => {
   const name = req.params.user;
   const reqUser = database.users.find(user => user.name === name);
-  res.json(reqUser);
+  if (reqUser) res.json(reqUser);
+  else res.status(404).json('No such user');
 });
 
-app.post('/signin', (req, res) => {
+app.post('/signin', async (req, res) => {
   const { name, password } = req.body;
   const reqUser = database.users.find(user => user.name === name);
-  if (reqUser && reqUser.password === password)
-    res.json({ status: 'Success', object: reqUser });
-  else res.json('Failure');
+  const hashVerified = await argon2.verify(reqUser?.passHash, password);
+  if (hashVerified) res.json({ status: 'Success', object: reqUser });
+  else res.status(400).json('Login Failed');
 });
 
-app.post('/register', (req, res) => {
+app.post('/register', async (req, res) => {
   const { name, password } = req.body;
   if (database.users.some(item => item.name === name))
-    res.json('Notgonnahappen');
+    res.json('Account Name Already Taken');
   else {
-    const newUser = new User(name, password);
+    const passHash = await argon2.hash(password);
+    const newUser = new User(name, passHash);
     database.users.push(newUser);
     res.json(newUser);
   }
@@ -50,8 +57,10 @@ app.post('/register', (req, res) => {
 app.put('/detected', (req, res) => {
   const { name, facesDetected } = req.body;
   const reqUser = database.users.find(user => user.name === name);
-  reqUser.facesDetected += facesDetected;
-  res.json(reqUser);
+  if (reqUser) {
+    reqUser.facesDetected += facesDetected;
+    res.json(reqUser);
+  } else res.status(400).send('No such user');
 });
 
 app.listen(3000);
