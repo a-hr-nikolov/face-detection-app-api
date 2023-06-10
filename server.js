c1f8309de82b688f864bf0cdabca4d1333fad128
a-hr-nikolov <a.hr.nikolov@gmail.com>
2023-06-10 15:10:35 +0300

    Route profile to database and FE

--- server.js
+++ server.js
@@ -16,43 +16,18 @@ const db = knex({
 
 const app = express();
 
-db.select('*')
-  .from('login')
-  .where('username', 'jonkata')
-  .then(([data]) => console.log(data));
-
-class User {
-  static createdCount = 0;
-  constructor(username, passHash) {
-    this.id = (++User.createdCount).toString().padStart(8, '0');
-    this.username = username;
-    this.passHash = passHash;
-    this.facesDetected = 0;
-    this.registrationDate = new Date();
-  }
-}
-
-const database = {
-  users: [],
-};
-
-argon2.hash('strongestpass').then(hash => {
-  database.users.push(new User('Jonkata', hash));
-});
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json(database.users);
-});
-
-app.get('/profile/:user', (req, res) => {
+app.get('/profile/:user', async (req, res) => {
   const username = req.params.user;
-  const reqUser = database.users.find(user => user.username === username);
 
-  if (reqUser) res.json(reqUser);
-  else res.status(404).json("The requested user doesn't exist.");
+  try {
+    let [user] = await db.select('*').from('users').where('username', username);
+    res.json(user);
+  } catch {
+    res.status(404).json("Couldn't get the requested user");
+  }
 });
 
 app.post('/signin', async (req, res) => {
@@ -99,13 +74,18 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.put('/detected', (req, res) => {
-  const { username, facesDetected } = req.body;
-  const reqUser = database.users.find(user => user.username === username);
-  if (reqUser) {
-    reqUser.facesDetected += facesDetected;
-    res.json(reqUser);
-  } else res.status(400).send('No such user');
+app.put('/detected', async (req, res) => {
+  const { username, detected } = req.body;
+
+  try {
+    let [user] = await db('users')
+      .where('username', username)
+      .update('detected', detected)
+      .returning('*');
+    res.json(user);
+  } catch {
+    res.status(400).send('No such user');
+  }
 });
 
 app.listen(3000);
